b965c92c4684d61fe871796a1ed6c73709380da0
Andy Marshall <ajmarshall@mail.com>
2018-12-31 05:49:04 +0000

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
-define ('DB_USER', "learntodrive");
-define ('DB_PASSWORD', "R1ngp1ece");
+define ('DB_USER', "******");
+define ('DB_PASSWORD', "******");
 define ('DB_DATABASE', "csv_db");
 define ('DB_HOST', "localhost");
 ?>
